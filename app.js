772aa6443e4cae5808ff4d35af6fc468b19984fb
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-02 09:33:11 +0530

    Password encrypted using mongoose-encryption

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27017/userDB')
+mongoose.connect(process.env.MONGODB_URL)
 .then(() => console.log('db connected successfully!'))
 .catch(err => console.log(err));
 
@@ -15,6 +17,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
