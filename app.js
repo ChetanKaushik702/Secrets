de47db8ccf5f77728b2e99b12058c25422e5dd4a
Chetan Kaushik <kaushikchetan199@gmail.com>
2021-11-02 09:15:14 +0530

    Username and Password only for authentication

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+mongoose.connect('mongodb://localhost:27017/userDB')
+.then(() => console.log('db connected successfully!'))
+.catch(err => console.log(err));
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = new mongoose.model('User', userSchema);
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -15,6 +28,37 @@ app.get('/register', (req, res) => {
 
 app.get('/login', (req, res) => {
     res.render('login');
+})
+
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+    newUser.save(err => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('secrets');
+        }
+    });
+})
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render('secrets');
+                }
+            }
+        }
+    })
 })
 
 
